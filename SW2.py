243c0548f4ac9afaf7205d5c4ca9e6bb5b01bf2d
abrochar <antoine.brochard@inria.fr>
2020-08-20 16:30:38 +0200

    sw ups

--- SW2.py
+++ SW2.py
@@ -27,88 +27,105 @@ def projection(X,L):
     return X_proj
 
 
-    def inverse_cdf(X_proj):
-        ''' Takes as input a tensor of size (L,B,C,N**2,2) corresponding to
-        sliced images. In X_proj, the last two dims correspond to the weights of
-        the Diracs and the respective positions.
-        '''
+def inverse_cdf(X_proj):
+    ''' Takes as input a tensor of size (L,B,C,N**2,2) corresponding to
+    sliced images. In X_proj, the last two dims correspond to the weights of
+    the Diracs and the respective positions.
+    '''
 
-        (L,B,C,N2) = X_proj[...,0].size()
+    (L,B,C,N2) = X_proj[...,0].size()
 
-        # We build a triangular matrix to transform the weights to the
-        # cumulative sum of weights
-        cum_sum_mat = torch.flip(torch.triu(torch.ones(N2,N2)), (0,1))  # (N^2,N2)
-        cum_sum_mat = cum_sum_mat.unsqueeze(0).unsqueeze(0).unsqueeze(0)
+    # We build a triangular matrix to transform the weights to the
+    # cumulative sum of weights
+    cum_sum_mat = torch.flip(torch.triu(torch.ones(N2,N2)), (0,1))  # (N^2,N2)
+    cum_sum_mat = cum_sum_mat.unsqueeze(0).unsqueeze(0).unsqueeze(0)
 
-        # We take the indices of ordered pixels positions
-        X_p_sorted_indices = torch.argsort(X_proj[...,1])  # (L,B,C,N**2)
+    # We take the indices of ordered pixels positions
+    X_p_sorted_indices = torch.argsort(X_proj[...,1])  # (L,B,C,N**2)
 
-        # We sort the weights and take the cum. sum
-        x_ = torch.index_select(X_proj[...,0], -1, X_p_sorted_indices)
-        x = torch.matmul(cum_sum_mat, x_.unsqueeze(-2)).view(L,B,C,N2)
+    # We sort the weights and take the cum. sum
+    x_ = torch.index_select(X_proj[...,0], -1, X_p_sorted_indices)
+    x = torch.matmul(cum_sum_mat, x_.unsqueeze(-2)).view(L,B,C,N2)
 
-        # We sort the pixel positions
-        y = torch.index_select(X_proj[...,1], -1, X_p_sorted_indices)
+    # We sort the pixel positions
+    y = torch.index_select(X_proj[...,1], -1, X_p_sorted_indices)
 
-        # Last dim: ordered pix. positions and respective cumsum of weights
-        X_p_sorted = torch.cat((y, x), dim=-1)
+    # Last dim: ordered pix. positions and respective cumsum of weights
+    X_p_sorted = torch.cat((y, x), dim=-1)
 
-        return X_p_sorted
-
-
-    def sw (X, Y, L):
-        ''' This function takes as input two batches of images, and returns
-        the batch sliced wasserstein distance between them. We concatenate
-        the icdf of X and Y, take the indexes of the sorted cumsum concatenation
-        and ...
-        '''
-        (B,C,N) = X[...,0].size()
+    return X_p_sorted
 
 
-        # At last dim, we add a new col. of +1/-1 for substraction
-        X_icdf = inverse_cdf(projection(X))  # (L,B,C,N**2,2)
-        X_icdf = torch.cat((X_icdf, torch.ones(L,B,C,N**2,1)), dim=-1)  # (L,B,C,N**2,3)
-        Y_icdf = inverse_cdf(projection(Y))  # (L,B,C,N**2,2)
-        Y_icdf = torch.cat((Y_icdf, -torch.ones(L,B,C,N**2,1)), dim=-1) # (L,B,C,N**2,3)
-
-        # We concatenate and take sorting indices
-        concat = torch.cat((X_icdf, Y_icdf), dim=-2)  # (L,B,C,2N^2,3)
-        indices = torch.argsort(concat[...,1], dim=-1)  # (L,B,C,2N^2)
+def sw (X, Y, L):
+    ''' This function takes as input two batches of images, and returns
+    the batch sliced wasserstein distance between them. We concatenate
+    the icdf of X and Y, take the indexes of the sorted cumsum concatenation
+    and ...
+    '''
+    (B,C,N) = X[...,0].size()
 
 
-        ##############
-        ### benoit ###
-        # get ordered cum_sum of X_i (called Ti in overleaf) and add 1 for last Ti
-        Ti = torch.index_select(concat[...,1], -1, indices)
-        Ti = torch.cat((Ti, torch.ones(L,B,C,1)), dim=-1)
-        # get order jumps (called wi in overleaf)
-        wi = torch.index_select(concat[...,0], -1, indices)
-        # get ordered coefs +1/-1
-        coef = torch.index_select(concat[...,-1], -1, indices)
-        # get square diff of icdf
-        diff = torch.cumsum(wi*coef,dim=-1)
-        square_diff = diff*diff * (Ti[1:] - Ti[:-1])
-        # SW
-        sw = square_diff.sum(dim=-1).mean(dim=0)
-        ##############
+    # At last dim, we add a new col. of +1/-1 for substraction
+    X_icdf = inverse_cdf(projection(X))  # (L,B,C,N**2,2)
+    X_icdf = torch.cat((X_icdf, torch.ones(L,B,C,N**2,1)), dim=-1)  # (L,B,C,N**2,3)
+    Y_icdf = inverse_cdf(projection(Y))  # (L,B,C,N**2,2)
+    Y_icdf = torch.cat((Y_icdf, -torch.ones(L,B,C,N**2,1)), dim=-1) # (L,B,C,N**2,3)
 
-        # Ordered cumsum of weights
-        diff_w = torch.index_select(concat[...,1], -1, indices)  # (L,B,C,2N^2)
-
-        # Ordered times
-        diff_p = torch.index_select(concat[...,0], -1, indices)  # (L,B,C,2N^2)
-
-        # We take the time lapses (posistions) to reintegrate with coeff +1/-1
-        z= torch.cat((torch.zeros(L,B,C,1), diff_p[...,:-1]), dim=-1)  # (L,B,C,2N^2)
-        diff_p_ = (diff_p - z)*torch.index_select(concat[...,2], -1, indices)  # (L,B,C,2N^2)
-
-        #cum_sum_mat = torch.flip(torch.triu(torch.ones(N2,N2)), (0,1))  # (N^2,N2)
-        #cum_sum_mat = cum_sum_mat.unsqueeze(0).unsqueeze(0).unsqueeze(0)
-        #diff1 = torch.matmul(cum_sum_mat, diff_p_.unsqueeze(-2)).view(L,B,C,N**2)
-
-        diff_p = torch.cumsum(diff_p_, dim=-1)  # (L,B,C,2N^2)
+    # We concatenate and take sorting indices
+    concat = torch.cat((X_icdf, Y_icdf), dim=-2)  # (L,B,C,2N^2,3)
+    indices = torch.argsort(concat[...,1], dim=-1)  # (L,B,C,2N^2)
 
 
-        diff = (diff0*diff1*diff1).sum(dim=-1).mean(dim=0)  # (B,C)
+    ##############
+    ### benoit ###
+    # get ordered cum_sum of X_i (called Ti in overleaf) and add 1 for last Ti
+    Ti = torch.index_select(concat[...,1], -1, indices)
+    Ti = torch.cat((Ti, torch.ones(L,B,C,1)), dim=-1)
+    # get order jumps (called wi in overleaf)
+    wi = torch.index_select(concat[...,0], -1, indices)
+    # get ordered coefs +1/-1
+    coef = torch.index_select(concat[...,-1], -1, indices)
+    # get square diff of icdf
+    diff = torch.cumsum(wi*coef,dim=-1)
+    square_diff = diff*diff * (Ti[1:] - Ti[:-1])
+    # SW
+    sw = square_diff.sum(dim=-1).mean(dim=0)
+    ##############
 
-        return diff, sw
+    # Ordered cumsum of weights
+    diff_w = torch.index_select(concat[...,1], -1, indices)  # (L,B,C,2N^2)
+
+    # Ordered times
+    diff_p = torch.index_select(concat[...,0], -1, indices)  # (L,B,C,2N^2)
+
+    # We take the time lapses (posistions) to reintegrate with coeff +1/-1
+    z= torch.cat((torch.zeros(L,B,C,1), diff_p[...,:-1]), dim=-1)  # (L,B,C,2N^2)
+    diff_p_ = (diff_p - z)*torch.index_select(concat[...,2], -1, indices)  # (L,B,C,2N^2)
+
+    #cum_sum_mat = torch.flip(torch.triu(torch.ones(N2,N2)), (0,1))  # (N^2,N2)
+    #cum_sum_mat = cum_sum_mat.unsqueeze(0).unsqueeze(0).unsqueeze(0)
+    #diff1 = torch.matmul(cum_sum_mat, diff_p_.unsqueeze(-2)).view(L,B,C,N**2)
+
+    diff_p = torch.cumsum(diff_p_, dim=-1)  # (L,B,C,2N^2)
+
+
+    diff = (diff0*diff1*diff1).sum(dim=-1).mean(dim=0)  # (B,C)
+
+    return diff, sw
+
+
+
+
+
+#### testing sw ####
+# B=1, C=1, L=4
+# SW between X and Y should return 1
+X = torch.zeros(1,1,128,128)
+X[:,:,0,0] = 1
+
+Y = torch.zeros(1,1,128,128)
+Y[:,:,0,1] = 1
+
+diff, sw = sw(X,Y,4)
+
+print(diff, sw)
