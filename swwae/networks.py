f48d811a8e2450be2dbb2b6265ff092dbb1e3582
benoitgaujac <benoit.gaujac@gmail.com>
2020-10-19 10:33:30 +0100

    3 layer mlp discr

--- swwae/networks.py
+++ swwae/networks.py
@@ -101,6 +101,11 @@ def theta_discriminator(opts, inputs, scope=None,
                                     256, init=opts['mlp_init'],
                                     scope='hid1/lin')
         layer_x = ops._ops.non_linear(layer_x,'leaky_relu')
+        # hidden 2
+        layer_x = ops.linear.Linear(opts, layer_x, np.prod(layer_x.get_shape().as_list()[1:]),
+                                    256, init=opts['mlp_init'],
+                                    scope='hid2/lin')
+        layer_x = ops._ops.non_linear(layer_x,'leaky_relu')
         # # final layer
         # outputs = ops.linear.Linear(opts, layer_x, np.prod(layer_x.get_shape().as_list()[1:]),
         #                             opts['sw_proj_num']*in_shape[-1],
