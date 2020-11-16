1b439d9b5ead07d36f9c29dd31fc15a6bb6f3ec4
benoitgaujac <benoit.gaujac@gmail.com>
2020-11-16 21:57:18 +0000

    cifar gen

--- swwae/net_archi.py
+++ swwae/net_archi.py
@@ -484,6 +484,31 @@ def cifar_fullconv_v2_critic(opts, inputs, scope=None, is_training=False, reuse=
     with tf.compat.v1.variable_scope(scope, reuse=reuse):
         # hidden 0
         layer_x = ops.conv2d.Conv2d(opts, layer_x, layer_x.get_shape().as_list()[-1],
+                                    output_dim=32, filter_size=3,
+                                    stride=2, scope='hid0/conv',
+                                    init=opts['conv_init'])
+        layer_x = ops._ops.non_linear(layer_x,'leaky_relu')
+        # hidden 1
+        layer_x = ops.deconv2d.Deconv2D(opts, layer_x, layer_x.get_shape().as_list()[-1],
+                                    output_shape=[batch_size,in_shape[0],in_shape[1],16],
+                                    filter_size=3,
+                                    stride=2, scope='hid1/deconv',
+                                    init=opts['conv_init'])
+        layer_x = ops._ops.non_linear(layer_x,'leaky_relu')
+        # final layer
+        outputs = ops.deconv2d.Deconv2D(opts, layer_x, layer_x.get_shape().as_list()[-1],
+                                    output_shape=[batch_size,]+in_shape, filter_size=1,
+                                    stride=1, scope='hid_final/deconv',
+                                    init= opts['conv_init'])
+
+    return outputs
+
+def cifar_fullconv_big_critic(opts, inputs, scope=None, is_training=False, reuse=False):
+    batch_size, in_shape =  tf.shape(inputs)[0], inputs.get_shape().as_list()[1:]
+    layer_x = inputs
+    with tf.compat.v1.variable_scope(scope, reuse=reuse):
+        # hidden 0
+        layer_x = ops.conv2d.Conv2d(opts, layer_x, layer_x.get_shape().as_list()[-1],
                                     output_dim=64, filter_size=4,
                                     stride=2, scope='hid0/conv',
                                     init=opts['conv_init'])
@@ -566,5 +591,5 @@ def celeba_fullconv_critic(opts, inputs, scope=None, is_training=False, reuse=Fa
 critic_archi = {'mlp': mlp_critic,
             'conv': conv_critic,
             'fullconv': {'mnist':cifar_fullconv_critic, 'svhn':cifar_fullconv_critic, 'cifar10':cifar_fullconv_critic, 'celebA':cifar_fullconv_critic},
-            'fullconv_v2': {'cifar10':cifar_fullconv_v2_critic, 'celebA':celeba_fullconv_critic}
+            'fullconv_v2': {'cifar10':cifar_fullconv_v2_critic, 'celebA':cifar_fullconv_v2_critic}
             }
