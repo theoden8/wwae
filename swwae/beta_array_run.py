7be6dd4b8cf0e23c209e5d8497cb3a73d62fedb7
benoitgaujac <benoit.gaujac@gmail.com>
2020-12-08 12:17:43 +0000

    celeba gen

--- swwae/beta_array_run.py
+++ swwae/beta_array_run.py
@@ -39,20 +39,8 @@ parser.add_argument("--lr", type=float,
                     help='learning rate size')
 parser.add_argument("--beta", type=float, default=0.,
                     help='beta')
-parser.add_argument("--slicing_dist", type=str, default='det',
-                    help='slicing distribution')
-parser.add_argument("--L", type=int, default=32,
-                    help='Number of slices')
 parser.add_argument("--gamma", type=float, default=1.,
                     help='weight for mass reg. in ground cost')
-parser.add_argument("--disc_freq", type=int, default=1,
-                    help='discriminator update frequency for aversarial sw')
-parser.add_argument("--disc_it", type=int, default=1,
-                    help='it. num. when updating discriminator for aversarial sw')
-parser.add_argument("--critic_archi", type=str, default='fullconv',
-                    help='archi for the critic')
-parser.add_argument("--critic_pen", type=float, default=1.,
-                    help='regularization weight for the critic')
 parser.add_argument("--id", type=int, default=0,
                     help='exp. config. id')
 parser.add_argument("--sigma_pen", action='store_true', default=False,
@@ -66,7 +54,25 @@ parser.add_argument('--save_model', action='store_false', default=True,
 parser.add_argument("--save_data", action='store_false', default=True,
                     help='save training data')
 parser.add_argument("--weights_file")
-
+## wgan cost
+parser.add_argument("--disc_freq", type=int, default=1,
+                    help='discriminator update frequency for aversarial sw')
+parser.add_argument("--disc_it", type=int, default=10,
+                    help='it. num. when updating discriminator for aversarial sw')
+parser.add_argument("--critic_archi", type=str, default='fullconv',
+                    help='archi for the critic')
+parser.add_argument("--critic_pen", type=float, default=1.,
+                    help='regularization weight for the critic')
+parser.add_argument("--critic_pretrain", action='store_true', default=False,
+                    help='pretrain cirtic')
+## wemd cost
+parser.add_argument("--orientation_num", type=int, default=8,
+                    help='oritenations number for waves base')
+## sw cost
+parser.add_argument("--slicing_dist", type=str, default='det',
+                    help='slicing distribution')
+parser.add_argument("--L", type=int, default=32,
+                    help='Number of slices')
 
 FLAGS = parser.parse_args()
 
@@ -94,40 +100,43 @@ def main():
     else:
         raise Exception('You must provide a data_dir')
 
-    # Set method param
-    opts['net_archi'] = FLAGS.net_archi
+    ## exp conf
+    betas = [1,5,10,50,100,250,500,1000]
+    coef_id = (FLAGS.id-1) % len(exp_config)
+    # exp_config = list(itertools.product(lr_decay,gammas, orientations))
+
+    ## Set method param
     opts['pen_enc_sigma'] = FLAGS.sigma_pen
     opts['beta_pen_enc_sigma'] = FLAGS.sigma_pen_val
 
-    # ground cost config
-    opts['cost'] = FLAGS.cost #l2, l2sq, l2sq_norm, l1, xentropy
-    opts['gamma'] = FLAGS.gamma
-    # wgan ground cost
-    opts['lambda'] = FLAGS.critic_pen
-    opts['wgan_critic_archi'] = FLAGS.critic_archi
-    # its = [1, 5]
-    betas = [1., 2., 4., 6. ,8., 10., 20.]
-    # exp_config = list(itertools.product(its, betas))
-    exp_config = betas
-    coef_id = (FLAGS.id-1) % len(exp_config)
-    # opts['d_updt_it'] = exp_config[coef_id][0]
-    # opts['d_updt_freq'] = exp_config[coef_id][0]
-    opts['d_updt_it'] = FLAGS.disc_it
-    opts['d_updt_freq'] = FLAGS.disc_freq
-    # sw ground cost
-    opts['sw_proj_num'] = FLAGS.L
-    opts['sw_proj_type'] = FLAGS.slicing_dist
-    # Model set up
+    ## Model set up
     opts['model'] = FLAGS.model
     opts['decoder'] = FLAGS.decoder
+    opts['net_archi'] = FLAGS.net_archi
     if opts['model'][-3:]=='VAE':
         opts['input_normalize_sym'] = False
     if FLAGS.batch_size:
         opts['batch_size'] = FLAGS.batch_size
     if FLAGS.lr:
         opts['lr'] = FLAGS.lr
-    opts['beta'] = exp_config[coef_id]
+    opts['lr_decay'] = False
+    opts['beta'] = betas[coef_id]
     # opts['beta'] = FLAGS.beta
+
+    ## ground cost config
+    opts['cost'] = FLAGS.cost
+    opts['gamma'] = FLAGS.gamma
+    # wgan ground cost
+    opts['pretrain_critic'] = FLAGS.critic_pretrain
+    opts['d_updt_it'] = FLAGS.disc_it
+    opts['d_updt_freq'] = FLAGS.disc_freq
+    opts['wgan_critic_archi'] = FLAGS.critic_archi
+    opts['lambda'] = FLAGS.critic_pen
+    # wemd ground cost
+    opts['orientation_num'] = FLAGS.orientation_num
+    # sw ground cost
+    opts['sw_proj_num'] = FLAGS.L
+    opts['sw_proj_type'] = FLAGS.slicing_dist
 
     # Create directories
     results_dir = 'results'
@@ -147,13 +156,21 @@ def main():
         exp_name += '_' + opts['sw_proj_type'] + '_L' + str(opts['sw_proj_num'])
         if opts['sw_proj_type']=='max-sw' or opts['sw_proj_type']=='max-gsw':
             exp_name += '_dfreq' + str(opts['d_updt_freq']) + '_dit' + str(opts['d_updt_it'])
-    if opts['cost']=='wgan':
+    elif opts['cost'][:4]=='wgan':
         # critic archi
         exp_name += '_' + opts['wgan_critic_archi']
         # critic training setup
-        exp_name += '_dfreq' + str(opts['d_updt_freq']) + '_dit' + str(opts['d_updt_it'])
+        exp_name += '_dfreq_' + str(opts['d_updt_freq'])
         # critic reg
-        exp_name += '_lbd' + str(opts['lambda'])
+        exp_name += '_dit_' + str(opts['d_updt_it'])
+        # critic reg
+        exp_name += '_l_' + str(opts['lambda'])
+    elif opts['cost']=='wemd':
+        exp_name += '_beta' + str(opts['beta'])
+        exp_name += '_gamma_' + str(opts['gamma'])
+        exp_name += '_L_' + str(opts['orientation_num'])
+    elif opts['cost']=='l2sq':
+        exp_name += '_lrdecay_' + str(opts['lr_decay'])
     if FLAGS.res_dir:
         exp_name += '_' + FLAGS.res_dir
     opts['exp_dir'] = os.path.join(out_subsubdir, exp_name)
@@ -171,7 +188,7 @@ def main():
 
     opts['it_num'] = FLAGS.num_it
     opts['print_every'] = int(opts['it_num'] / 10.)
-    opts['evaluate_every'] = int(opts['it_num'] / 100.)
+    opts['evaluate_every'] = int(opts['it_num'] / 50.)
     opts['save_every'] = 10000000000
     opts['save_final'] = FLAGS.save_model
     opts['save_train_data'] = FLAGS.save_data
